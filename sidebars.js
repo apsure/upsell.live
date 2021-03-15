c409d432b55e1c2dc75f8cbdcd503c5d238c3409
Jeff Seifert <jeff.seifert@apsure.com>
2021-03-15 15:00:02 -0400

    Add page about browser support

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
-    Guides: ['introduction', 'creating_upsells', 'reordering_upsells', 'deploying_upsells', 'troubleshooting'],
+    Guides: ['introduction', 'creating_upsells', 'reordering_upsells', 'deploying_upsells', 'troubleshooting', 'browser_support'],
     Advanced: [ 'advanced/cart_embedding', 'advanced/additional_prompting' ]
   },
 };
